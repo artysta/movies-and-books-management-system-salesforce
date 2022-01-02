947390962a5f7a20306b9833a2e99243a24046b7
Adrian Kurek <adrian.kurek93@gmail.com>
2022-01-02 16:19:49 +0100

    Add aura component for authors

---
+++ src/aura/authorsList/authorsListController.js
@@ -0,0 +1,21 @@
+({
+    getAuthors: function (component, event, helper) {
+        component.set('v.columns', [
+            {label: 'Id', fieldName: 'Id', type: 'text'},
+            {label: 'Name', fieldName: 'Name', type: 'text'},
+            {label: 'Birthplace', fieldName: 'MBMS_Birthplace__c', type: 'text'},
+            {label: 'Date of Birth', fieldName: 'MBMS_Date_of_Birth__c', type: 'date'},
+            {label: 'Date of Death', fieldName: 'MBMS_Date_of_Death__c', type: 'date'}
+        ]);
+        
+        var action = component.get('c.getAuthorsList');
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                component.set('v.authors', response.getReturnValue());
+            }
+        });
+        
+        $A.enqueueAction(action);
+    }
+})
