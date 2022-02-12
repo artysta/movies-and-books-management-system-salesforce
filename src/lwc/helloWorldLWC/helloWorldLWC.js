50666e3bc356721e122ebd18297e4fb2c7d9dbc2
Adrian Kurek <adrian.kurek93@gmail.com>
2022-02-12 08:22:12 +0100

    Remove unnecessary component

--- src/lwc/helloWorldLWC/helloWorldLWC.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class HelloWorld extends LightningElement {
-    movieTitle = 'Kill Bill';
-    photoUrl = 'https://cdn.pixabay.com/photo/2022/01/23/13/04/sparrow-6960807_960_720.jpg';
-}
